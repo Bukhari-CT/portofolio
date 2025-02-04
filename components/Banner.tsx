337a11e78e34dac1cc5f3adf51028f7b2ca1b4bf
Faisal Bukhari <faisalbukhari101@outlook.com>
2025-02-04 10:18:51 +0000

    revamp: refined banner heading

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,14 +5,6 @@ const Banner = () => {
       id="home"
       className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
     >
-      <motion.h3
-        initial={{ y: 10, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.6 }}
-        className="text-lg font-titleFont tracking-wide text-center text-textDarkBlue"
-      >
-        Hi, my name is
-      </motion.h3>
       <motion.h1
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
@@ -20,9 +12,6 @@ const Banner = () => {
         className="text-4xl lgl:text-6xl font-titleFont text-center font-semibold flex flex-col"
       >
         <span className="font-Sevillana">Building things for web</span>
-        <span className="text-textDark mt-2 lgl:mt-4">
-          I build things for the web.
-        </span>
       </motion.h1>
       <motion.p
         initial={{ y: 10, opacity: 0 }}
